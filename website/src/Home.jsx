41ae7a3b66b2b3a4db6e1dd4af3fafb3e65264a8
Frankibinmo <frankibinmo@gmail.com>
2025-03-07 16:04:39 +0100

    customHook pending and error

--- website/src/Home.jsx
+++ website/src/Home.jsx
@@ -4,23 +4,36 @@ import BlogList from "./BlogList";
 const HomePage = () => {
   const [blogs, setBlogs] = useState(null);
 
+  const [isPending, setIsPending] = useState(true)
+
+  const [error, setError] = useState(null)
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
 
     setBlogs(newBlogs);
   };
-
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
-  });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if(!res.ok)
+           throw Error('could not access the data')
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        })
+        .catch( err =>{
+          setError(error.message)
+        })
+    }, 1000)
+  },[]);
   return (
     <div className="HomePage">
+      {error && <div>{error}</div>}
+      {isPending && <div>loading...</div>}
       {blogs && (
         <BlogList
           blogs={blogs}
