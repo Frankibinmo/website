b8ac1e8e8d40b13918fa09f326331d39e16f3965
Frankibinmo <frankibinmo@gmail.com>
2025-03-07 01:38:34 +0100

    fetching data with useeffect with json server

--- website/src/Home.jsx
+++ website/src/Home.jsx
@@ -1,26 +1,35 @@
-import {useState } from 'react';
-import BlogList from './BlogList';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 const HomePage = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'my new website', body: 'this site belongs to frank', author:'Ode', id: 1},
-        {title: 'my new website', body: 'this site belongs to frank', author:'frank', id: 2},
-        {title: 'my new website', body: 'this site belongs to frank', author:'shalom', id: 3},
-        {title: 'my new website', body: 'this site belongs to frank', author:'frank', id: 4},
-        {title: 'my new website', body: 'this site belongs to frank', author:'Ode', id: 5},
-    ]);
+  const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id  !== id)
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
 
-      setBlogs(newBlogs);
-    }
- 
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  });
   return (
-    <div className='HomePage'>
-     <BlogList blogs={blogs} title="All blogs!!" handleDelete={handleDelete}/>
+    <div className="HomePage">
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All blogs!!"
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
